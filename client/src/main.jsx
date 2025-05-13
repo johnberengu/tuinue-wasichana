f5d97b4c7a341af0dad561ec179acfd259c73ce5
eli <john.berengu@student.moringaschool.com>
2025-05-13 14:38:28 +0300

    changes in index.html

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 import { Provider } from 'react-redux';
-import store from './store'; // updated import path to correct store.js location
+import store from './store'; 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
